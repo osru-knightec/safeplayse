396a268afee2589cfef2b6c7e98aea0332ddddef
Oskar Rubensson <107415465+osru-knightec@users.noreply.github.com>
2024-04-15 19:20:12 +0200

    Add pages:build script to package.json and define runtime variable in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
 
+export const runtime = "edge";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
