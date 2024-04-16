e3ca95e451cfb0b14bd3e0ba25b4275ad4192d5a
Oskar Rubensson <107415465+osru-knightec@users.noreply.github.com>
2024-04-16 11:48:20 +0200

    Added my site to the nextjs project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,65 @@
+"use client";
 import Image from "next/image";
-export const runtime = "edge";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By <Image src="/vercel.svg" alt="Vercel Logo" className="dark:invert" width={100} height={24} priority />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+    <main className="flex min-h-screen mx-auto flex-col  from-orange-300 to-orange-500 bg-gradient-radial">
+      <header className="w-full h-24 flex items-center flex-row px-8 border-b-2 border-b-slate-200 bg-white">
+        <Image src="/splash.png" alt="SafePlay+" width={200} height={24} />
+      </header>
+      <div className="pb-32 px-12">
+        <section className="flex flex-row justify-around items-center flex-wrap gap-32 mt-32">
+          <div>
+            <h1 className="text-5xl font-semibold">
+              Enkel lekplatsbesiktning,
+              <br />
+              direkt i mobilen
+            </h1>
+            <div className="flex flex-row items-center gap-16 mt-12">
+              <a href="https://apps.apple.com/se/app/safeplay/id6445820584" target="_blank">
+                <Image src="/app_store.svg" alt="App Store" width={200} height={60} />
+              </a>
+              <a href="https://play.google.com/store/apps/details?id=se.oskarrubensson.safeplayplus" target="_blank">
+                <Image src="/google_play.png" alt="Google Play" width={200} height={60} />
+              </a>
+            </div>
+          </div>
+          <div className="bg-white p-2 rounded-md shadow-orange-500 shadow-md border border-orange-500">
+            <Image
+              src="/man_by_playground.png"
+              alt=""
+              width={600}
+              height={600}
+              className="rounded-md border border-gray-700"
+            />
+          </div>
+        </section>
+        <section className="flex flex-row justify-around items-center flex-wrap mt-32">
+          <div className="key-box">
+            <h2 className="text-3xl font-semibold">Snabbare</h2>
+            <hr className="h-[2px] bg-orange-200 my-2" />
+            <p>När du lämnar lekplatsen har du ett färdigt och snyggt protokoll som är redo att skickas till kund</p>
+          </div>
+          <div className="key-box">
+            <h2 className="text-3xl font-semibold">Säkrare</h2>
+            <hr className="h-[2px] bg-orange-200 my-2" />
+            <p>Alla protokoll du någonsin skapat finns säkerhetskopierade och sparade i appen</p>
+          </div>
+          <div className="key-box">
+            <h2 className="text-3xl font-semibold">Proffsigare</h2>
+            <hr className="h-[2px] bg-orange-200 my-2" />
+            <p>Genom att lägga in ditt företags branding ser dina protokoll snygga, stilrena och proffsiga ut</p>
+          </div>
+        </section>
+        <section className="mx-auto w-1/2 bg-white rounded-md p-4 mt-40">
+          <h2 className="text-3xl font-semibold pb-4">Kontakt</h2>
+          <p>
+            Om du har frågor eller funderingar är du varmt välkommen att kontakta oss på{" "}
+            <a href="mailto:oskar@safeplay.se" className="text-orange-500">
+              oskar@safeplay.se
+            </a>
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </section>
       </div>
     </main>
   );
